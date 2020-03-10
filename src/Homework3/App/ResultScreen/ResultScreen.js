bb7d56090fa3870d48e40529c40b22bfb273109a
NikitaPO <gigzim345@gmai.com>
2020-03-10 22:07:57 +0300

    feature: have added result screen

--- src/Homework3/App/ResultScreen/ResultScreen.js
+++ src/Homework3/App/ResultScreen/ResultScreen.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class ResultScreen extends Component {
   render() {
-    return 3;
+    let orderNumber = "#" + parseInt(Math.random() * 899999 + 100000);
+
+    return (
+      <h1 className="header-title">
+        Your order {orderNumber} has been placed!
+      </h1>
+    );
   }
 }
