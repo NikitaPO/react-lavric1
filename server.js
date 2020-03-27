8b5f089ac4a848f220695581c73a4f75b536fec6
NikitaPO <gigzim345@gmai.com>
2020-03-28 02:19:58 +0300

    server.js was added

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const favicon = require("express-favicon");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+// // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+// app.use(favicon(__dirname + "/dist/favicon.ico"));
+
+// //здесь наше приложение отдаёт статику
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "dist")));
+
+// //простой тест сервера
+// app.get("/ping", function(req, res) {
+//   return res.send("pong");
+// });
+
+//обслуживание html
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server has been started... http://localhost:8080/");
+});
