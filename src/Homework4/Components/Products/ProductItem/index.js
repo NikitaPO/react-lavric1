da7f89c2532461ad5ef3fc7c079b4fcb34ca50de
NikitaPO <gigzim345@gmai.com>
2020-03-27 03:48:17 +0300

    split product to components

---
+++ src/Homework4/Components/Products/ProductItem/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Row, Col, Image, ListGroup } from "react-bootstrap";
+
+export default function(props) {
+  return (
+    <div>
+      <h1 className="header-title">{props.title}</h1>{" "}
+      <Row>
+        <Col>
+          <Image src={props.img} thumbnail />
+        </Col>
+        <Col>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <b>Price:</b> {props.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <b>Rest:</b> {props.rest}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <b>Description:</b>
+              <p>{props.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
+}
--- src/Homework4/Containers/Product/index.js
+++ src/Homework4/Containers/Product/index.js
@@ -1,41 +1,30 @@
 import React from "react";
-import { Row, Col, Image, ListGroup } from "react-bootstrap";
 import { inject, observer } from "mobx-react";
 import Error404 from "~com/Errors/404";
+import ProductItem from "~com/Products/ProductItem";
 
-// @inject('stores')
+@inject("stores")
+@observer
+class Product extends React.Component {
+  render() {
+    const productsStore = this.props.stores.productsStore;
+    const productId = this.props.match.params.id;
+    const product = productsStore.getProduct(productId);
 
-export default function(props) {
-  // const productsStore = this.props.stores.productsStore;
-
-  const productId = props.match.params.id;
-  const product = productsStore.getProduct(productId);
-  const productPage = product ? (
-    <div>
-      <h1 className="header-title">{product.title}</h1>{" "}
-      <Row>
-        <Col>
-          <Image src={product.img} thumbnail />
-        </Col>
-        <Col>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <b>Price:</b> {product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <b>Rest:</b> {product.rest}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <b>Description:</b>
-              <p>{product.description}</p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-    </div>
-  ) : (
-    <Error404 />
-  );
-
-  return productPage;
+    if (product === null) {
+      return <Error404 />;
+    } else {
+      return (
+        <ProductItem
+          title={product.title}
+          img={product.img}
+          title={product.title}
+          rest={product.rest}
+          description={product.description}
+        />
+      );
+    }
+  }
 }
+
+export default Product;
