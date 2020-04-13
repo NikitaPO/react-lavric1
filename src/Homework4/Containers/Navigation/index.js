d45a1fad1a118973a0ca84deddc716ed47b7b299
NikitaPO <gigzim345@gmai.com>
2020-04-13 12:42:13 +0300

    active links, more simple solution

--- src/Homework4/Containers/Navigation/index.js
+++ src/Homework4/Containers/Navigation/index.js
@@ -1,57 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ListGroup } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import withStore from "~/Hocs/withStore";
 import { routesMap } from "~/Routes";
 
 const Navigation = () => {
-  const listGroup = React.createRef();
-  const [checkLinks, setCheckLinks] = useState(false);
-
-  useEffect(() => {
-    const links = listGroup.current.childNodes;
-    links.forEach((link) => {
-      link.firstChild.classList.contains("active")
-        ? link.classList.add("active")
-        : link.classList.remove("active");
-    });
-    setCheckLinks(false);
-  }, [checkLinks]);
-
-  const handleClick = () => setCheckLinks(true);
-
   return (
-    <ListGroup className="mt-4" ref={listGroup}>
-      <ListGroup.Item>
-        <NavLink
-          to={routesMap.products}
-          onClick={handleClick}
-          className="navigation-link"
-          exact
-        >
-          Products
-        </NavLink>
-      </ListGroup.Item>
-      <ListGroup.Item>
-        <NavLink
-          to={routesMap.cart}
-          onClick={handleClick}
-          className="navigation-link"
-          exact
-        >
-          Cart
-        </NavLink>
-      </ListGroup.Item>
-      <ListGroup.Item>
-        <NavLink
-          to={routesMap.order}
-          onClick={handleClick}
-          className="navigation-link"
-          exact
-        >
-          Order
-        </NavLink>
-      </ListGroup.Item>
+    <ListGroup className="mt-4">
+      <NavLink
+        to={routesMap.products}
+        className="list-group-item navigation-link"
+        exact
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to={routesMap.cart}
+        className="list-group-item navigation-link"
+        exact
+      >
+        Cart
+      </NavLink>
+      <NavLink
+        to={routesMap.order}
+        className="list-group-item navigation-link"
+        exact
+      >
+        Order
+      </NavLink>
     </ListGroup>
   );
 };
